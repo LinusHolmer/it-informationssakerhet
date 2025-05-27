cba9ef496b4cb7b0abc0197c025db4b41090d35e
LinusHolmer <linus.holmer@stud.sti.se>
2025-05-27 12:12:19 +0200

    lektion 5 redo att koda

--- src/main/java/com/example/itinformationssakerhet/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.itinformationssakerhet.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
